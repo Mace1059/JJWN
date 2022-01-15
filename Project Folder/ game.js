aeeb9401af5018039fee0250e472ab06b899a2a1
wbalkan <will.balkan@comcast.net>
2022-01-15 00:23:35 -0500

    undo

--- Project Folder/ game.js
+++ Project Folder/ game.js
@@ -17,23 +17,3 @@ function myFunction() {
 
 
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-  function createParagraph() {
-    const para = document.createElement('p');
-    para.textContent = 'ayo shawty you clicked the button!';
-    document.body.appendChild(para);
-  }
-
-  const buttons = document.querySelectorAll('button');
-
-  for (const button of buttons) {
-    button.addEventListener('click', createParagraph);
-  }
-
-function willFunction() {
-  alert("we out here");
-}
-
-
-});
