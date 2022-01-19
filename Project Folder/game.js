f0a621f98184fa9f770517e2641452c8213ebf4f
Mace1059 <jackmacy123@gmail.com>
2022-01-19 14:51:27 -0500

    New code for name entry

--- Project Folder/game.js
+++ Project Folder/game.js
@@ -1,15 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    function createParagraph() {
-      const para = document.createElement('p');
-      para.textContent = 'You clicked the button!';
-      document.body.appendChild(para);
-    }
+// document.addEventListener('DOMContentLoaded', () => {
+// const buttons = document.querySelectorAll('button');
 
-    const buttons = document.querySelectorAll('button');
+// for (const button of buttons) {
+//   button.addEventListener('click', chooseName);
+// }
 
-    for (const button of buttons) {
-      button.addEventListener('click', createParagraph);
-    }
+
+let playerList = ['player1', 'player2', 'player3']
+var PlayerCount = 0
+var Questions = 0;
+var TimeRemaining = 0;
+
+function chooseName(){
+    var name = document.getElementById("nameEntryBox").value;
+    const para = document.createElement('p');
+    para.textContent = 'Your name is ' + name;
+    document.body.appendChild(para);
+}
+
+
+
+
+// function createParagraph() {
+//     const para = document.createElement('p');
+//     para.textContent = 'You clicked the button!';
+//     document.body.appendChild(para);
+// }
+
+
   
 function myFunction() {
   alert("let's get sturdy");
