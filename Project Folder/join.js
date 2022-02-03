7778f933857f2d0b152a238fdfb395ed61a4e127
Mace1059 <jackmacy123@gmail.com>
2022-02-02 21:43:44 -0500

    temporary code for JV

--- Project Folder/join.js
+++ Project Folder/join.js
@@ -1,1 +1,18 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: { origin: "*"}})
 
+
+app.get('/join', (req, res) => {
+    res.render('join')
+})
+
+
+server.listen(3001, () => {
+    console.log('test');
+})
+
+io.on('connection', (socket) => {
+    console.log
+});
