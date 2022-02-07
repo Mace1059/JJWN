0b29c4f7100ef4b0b1b212eb03064a65c2dadd0c
JackVH3 <jackvanhooser@gmail.com>
2022-02-06 22:45:34 -0500

    join button

--- Project Folder/join.js
+++ Project Folder/join.js
@@ -1,1 +1,21 @@
+function chooseName(){
+    var name = document.getElementById("nameEntryBox").value;
+    if (name == ''){
+      return
+    }
+    else {
+      // Figure out how to append instead of replacing element
+      document.getElementById('nameDisplay').innerHTML = "Name: " + name;
+    }
+}
 
+function chooseID(){
+    var id = document.getElementById("IDEntryBox").value;
+    if (id == ''){
+      return
+    }
+    else {
+      // Figure out how to append instead of replacing element
+      document.getElementById('IDDisplay').innerHTML = "ID: " + id;
+    }
+}
