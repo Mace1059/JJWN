a460858301153c463b631c5bc6d9436f9b875017
Mace1059 <jackmacy123@gmail.com>
2022-03-08 14:43:51 -0500

    testing animation

--- Project Folder/game.js
+++ Project Folder/game.js
@@ -15,10 +15,14 @@ var index;
 var questionsCorrect = 0;
 var questionsAnswered = 0;
 
-var finalRoundScore = 1;
+var finalRoundScoreLeft = 1;
+var finalRoundScoreRight = 1;
+
 var finalRound = false;
+
 //=====================================================================================
 
+// Player class for name/score, used in playerMap with name as key
 class Player {
   constructor(name, score) {
     this.name = name;
@@ -32,6 +36,28 @@ class Player {
   }
 }
 
+//create 
+const playerMap = new Map();
+var winnerPlayerMap = new Map();
+
+playerMap.set('name1', new Player('name1', 100));
+// playerMap.set('name2', new Player('name2', 200));
+// playerMap.set('name3', new Player('name3', 300));
+// playerMap.set('name4', new Player('name4', 400));
+// playerMap.set('name5', new Player('name5', 500));
+// playerMap.set('name6', new Player('name6', 600));
+// playerMap.set('name7', new Player('name7', 700));
+// playerMap.set('name8', new Player('name8', 100));
+// playerMap.set('name9', new Player('name9', 200));
+// playerMap.set('name10', new Player('name10', 300));
+// playerMap.set('name11', new Player('name11', 400));
+// playerMap.set('name12', new Player('name12', 500));
+// playerMap.set('name13', new Player('name13', 600));
+// playerMap.set('name14', new Player('name14', 600));
+// playerMap.set('name15', new Player('name15', 600));
+
+
+// Sort players still in contention by their score, returns list of names in order by score
 function sortPlayersByScore(pMap) {
   var points = [];
   for (let [key, value] of pMap) {
@@ -52,34 +78,15 @@ function sortPlayersByScore(pMap) {
 }
 
 
-const playerMap = new Map();
-var winnerPlayerMap = new Map();
-
-playerMap.set('name1', new Player('name1', 100));
-playerMap.set('name2', new Player('name2', 200));
-// playerMap.set('name3', new Player('name3', 300));
-// playerMap.set('name4', new Player('name4', 400));
-// playerMap.set('name5', new Player('name5', 500));
-// playerMap.set('name6', new Player('name6', 600));
-// playerMap.set('name7', new Player('name7', 700));
-// playerMap.set('name8', new Player('name8', 100));
-// playerMap.set('name9', new Player('name9', 200));
-// playerMap.set('name10', new Player('name10', 300));
-// playerMap.set('name11', new Player('name11', 400));
-// playerMap.set('name12', new Player('name12', 500));
-// playerMap.set('name13', new Player('name13', 600));
-// playerMap.set('name14', new Player('name14', 600));
-// playerMap.set('name15', new Player('name15', 600));
-
-// var sortedList = sortPlayersByScore(playerMap);
-
-
 // tp = total players, winnerlist = list of players still in contention 
+// generates bracket based on rankings
+// in case of odd number, randomize between 2 and n-1 and add to 1 v n matchup
 function bracketGenerator(mlist){
   
   tp = mlist.length;
   let mbracket = new Array(); // create an empty array of length tp
 
+  // i and j are moving indices for matchmaking
   i = 0;
   j = mlist.length-1;
   
@@ -118,7 +125,8 @@ function bracketGenerator(mlist){
 //=====================================================================================
 
 
-
+// utility function to randomly shuffle array
+// used in code to randomize location of 
 function shuffle(array) {
   let currentIndex = array.length,  randomIndex;
 
@@ -138,28 +146,20 @@ function shuffle(array) {
   // Keep track of the "correct" index in the array with index variable
   // See button section for more comments
   for (let i = 0; i < array.length; i++) {
-    if (question.correct == 1)
-    {
-      if (array[i] == question.a1)
-      {
+    if (question.correct == 1) {
+      if (array[i] == question.a1) {
         index = i;
       }
-    } else if (question.correct == 2)
-    {
-      if (array[i] == question.a2)
-      {
+    } else if (question.correct == 2) {
+      if (array[i] == question.a2){
         index = i;
       }
-    } else if (question.correct == 3)
-    {
-      if (array[i] == question.a3)
-      {
+    } else if (question.correct == 3) {
+      if (array[i] == question.a3) {
         index = i;
       }
-    } else if (question.correct == 4)
-    {
-      if (array[i] == question.a4)
-      {
+    } else if (question.correct == 4) {
+      if (array[i] == question.a4) {
         index = i;
       }
     }
@@ -171,18 +171,6 @@ function shuffle(array) {
 // Returns true if chosen answer is the correct one, false otherwise
 function checkAnswer(int, index) {
   return (int == index); // correct is boolean, need index of right answer
-}
-
-
-// random will nonsense
-function willFunction() {
-  var order = document.getElementById("menuSelection").value;
-  let hiddenMenu = ['beeschurger', 'big chungo', 'toes', 'sturdy'];
-  if (hiddenMenu.includes(order)) {
-    alert('dw bout it dawg we shlanging out some mf ' + order);
-  } else {
-    alert('mf that is not on the hidden menu');
-  }
 }
 
 
@@ -224,10 +212,12 @@ function updateTimer(int){
 
 function bar(mtime) {
   clearInterval(startBar);
-  var width = 0;
+  var width = 1000;
   startBar = setInterval(frame, 10);
   function frame() {
+    console.log('test')
     if (width >= 100) {
+      console.log('pussy')
       clearInterval(startBar);
     } else {
       width = width + (1/mtime); 
@@ -235,6 +225,10 @@ function bar(mtime) {
     }
   }
 }
+
+// function checkBar(){
+//   if barwidth ? 
+// }
 
 function chooseName(){
   pname = document.getElementById("nameEntryBox").value;
@@ -272,8 +266,32 @@ function determineWinner(mlist){
   matchmaking(winnerPlayerMap);
 }
 
+//change width of rectangle in final bar based on 
+function fillRectangleFunction(int){
+  var canvas = document.getElementById("finalBar");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#FFD700";
+  if (int===0){
+    middle = canvas.width/2
+    barwidth = middle
+  }
+  else {
+    barwidth += int;
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(0, 0, barwidth, canvas.height);
+}
+
+function simulateFinalRound(){
+  startSimulate = setInterval(function(){
+    int = -2
+    fillRectangleFunction(int)
+  }, 1000);
+}
+
 function finishGame(){
-  
+  document.getElementById('revealDisplay').style.display = "none";  
+  document.getElementById('gameDisplay').style.display = "none";
 }
 
 
@@ -301,7 +319,6 @@ function matchmaking(pmap){
   if (size > 3){
     bar(5)
     setTimeout(function() { updateTimer(TIME); }, 5000)
-    //move to updateTimer
   }
   else {
     finalRound = true;
@@ -314,32 +331,13 @@ function matchmaking(pmap){
   }
 }
 
-
-function fillRectangleFunction(int){
-  var canvas = document.getElementById("finalBar");
-  var ctx = canvas.getContext("2d");
-  ctx.fillStyle = "#FFD700";
-  if (int===0){
-    barwidth = canvas.width/2
-  }
-  else {
-    barwidth += int;
-  }
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillRect(0, 0, barwidth, canvas.height);
-}
-
-function simulateFinalRound(){
-  startSimulate = setInterval(function(){
-    int = -3
-    fillRectangleFunction(int)
-  }, 1000);
-}
-
-
+// called by answer buttons
+// reveals message and "Next Question" button
 function answerSubmit(int){
   clearInterval(startScoreTimer)
   correct = checkAnswer(int - 1, index);
+
+  // test if final round, if so, skip next question screen and show new question
   if (finalRound) {
     if (correct == true) {
       console.log(finalRoundScore)
@@ -349,8 +347,6 @@ function answerSubmit(int){
   }
   else {
     //Checks to see if answer index matches correct index
- 
-
     document.getElementById('revealDisplay').style.display = "block";  
     document.getElementById('questionButtons').style.display = "none";
     document.getElementById('message').style.display = 'block';
@@ -373,7 +369,7 @@ function answerSubmit(int){
   }
 }
 
-
+// function to show first/next question on "Next Question" button press
 function showQuestion() {
   var answerList = [question.a1, question.a2, question.a3, question.a4];
   var shuffledAnswerList = shuffle(answerList)
@@ -394,4 +390,20 @@ function showQuestion() {
   document.getElementById('answer4').innerHTML = shuffledAnswerList[3];
   
   speedScoreTimer()
+}
+
+
+
+
+
+
+// random will nonsense
+function willFunction() {
+  var order = document.getElementById("menuSelection").value;
+  let hiddenMenu = ['beeschurger', 'big chungo', 'toes', 'sturdy'];
+  if (hiddenMenu.includes(order)) {
+    alert('dw bout it dawg we shlanging out some mf ' + order);
+  } else {
+    alert('mf that is not on the hidden menu');
+  }
 }
--- Project Folder/gameElementTest.js
+++ Project Folder/gameElementTest.js
@@ -1,1 +1,11 @@
+function createElement(type, mclass, mid, mcontent) {
+    const e = document.createElement('div');
+    document.body.appendChild(e);
+    e.textContent = mcontent;
+    e.setAttribute('class', mclass);
+    e.setAttribute("id", mid);
+    console.log(type, mclass, mid, mcontent)
+}
+createElement('div', 'matchupBox', 'mb1', 'Fuck you pussy1')
+createElement('div', 'matchupBox', 'mb2', 'Fuck you pussy2')
 
